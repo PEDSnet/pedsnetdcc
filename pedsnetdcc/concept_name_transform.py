027ff0af55bcaf214672ec6329fff8f1d750cfd5
Kevin Murphy <murphyke@email.chop.edu>
2016-06-20 12:54:33 -0400

    Implement modify_metadata for ConceptNameTransform

--- pedsnetdcc/concept_name_transform.py
+++ pedsnetdcc/concept_name_transform.py
@@ -1,14 +1,33 @@
+import hashlib
+
+from sqlalchemy.schema import Column, Index
+
 from pedsnetdcc import VOCAB_TABLES
 from pedsnetdcc.abstract_transform import Transform
 
 
-def modify_table(table):
+# Oracle's identifier length maximum is 30 bytes, so we must limit
+# object identifier length (e.g. for columns and indexes).
+NAME_LIMIT = 30
+
+
+def make_index_name(table_name, column_name):
     """
-    pass
-    :param table:
-    :return:
+    Create an index name for a given table/column combination with
+    a NAME_LIMIT-character (Oracle) limit.  The table/column combination
+    `provider.gender_source_concept_name` results in the index name
+    `pro_gscn_ae1fd5b22b92397ca9_ix`.  We opt for a not particularly
+    human-readable name in order to avoid collisions, which are all too
+    possible with columns like provider.gender_source_concept_name and
+    person.gender_source_concept_name.
     """
-    pass
+    table_abbrev = table_name[:3]
+    column_abbrev = ''.join([x[0] for x in column_name.split('_')])
+    md5 = hashlib.md5(
+        '{}.{}'.format(table_name, column_name).encode('utf-8')).hexdigest()
+    hashlen = NAME_LIMIT - (len(table_abbrev) + len(column_abbrev) +
+                            3 * len('_') + len('ix'))
+    return '_'.join([table_abbrev, column_abbrev, md5[:hashlen], 'ix'])
 
 
 class ConceptNameTransform(Transform):
@@ -18,10 +37,7 @@ class ConceptNameTransform(Transform):
 
         Requirements: `concept` table exists.
 
-        :param sqlalchemy.MetaData metadata:
-        :param str table_name:
-        :param sqlalchemy.Select select:
-        :rtype: sqlalchemy.Select
+        See Transform.modify_select for signature.
         """
         concept = metadata.tables['concept']
 
@@ -31,11 +47,31 @@ class ConceptNameTransform(Transform):
                 concept_alias = concept.alias()
                 new_col = concept_alias.c.concept_name.label(new_name)
                 join = join.outerjoin(concept_alias,
-                                                concept_alias.c.concept_id
-                                                == col)
+                                      concept_alias.c.concept_id
+                                      == col)
                 select = select.column(new_col)
 
+        print("modify_select: ", hasattr(cls, 'modify_select'))
+        print("nonexistent: ", hasattr(cls, 'nonexistent'))
+
         return select, join
+
+    @classmethod
+    def modify_table(cls, metadata, table):
+        """Helper function to apply the transformation to a table in place.
+        :param sqlalchemy.schema.MetaData metadata:
+        :param sqlalchemy.schema.Table table:
+        :return: None
+        """
+        concept_name = metadata.tables['concept'].c.concept_name
+
+        orig_columns = table.c
+        for col in orig_columns:
+            if col.name.endswith('_concept_id'):
+                new_col_name = col.name.replace('_concept_id', '_concept_name')
+                new_col = Column(new_col_name, concept_name.type)
+                table.append_column(new_col)
+                Index(make_index_name(table.name, new_col_name), new_col)
 
     @classmethod
     def modify_metadata(cls, metadata):
@@ -43,17 +79,12 @@ class ConceptNameTransform(Transform):
 
         Requirements: `concept` table exists.
 
-        :param sqlalchemy.MetaData metadata:
-        :param str table_name:
-        :param sqlalchemy.Select select:
-        :rtype: sqlalchemy.Select
+        See Transform.modify_metadata for signature.
         """
-        concept = metadata.tables['concept']
-
-        for table in metadata.tables:
+        for table in metadata.tables.values():
             if table in VOCAB_TABLES:
                 continue
 
-            modify_table(table)
+            cls.modify_table(metadata, table)
 
         return metadata
--- pedsnetdcc/concept_name_transform_test.py
+++ pedsnetdcc/concept_name_transform_test.py
@@ -1,13 +1,18 @@
 import re
+import unittest
+
 from sqlalchemy import *
 from sqlalchemy.dialects import postgresql
-import unittest
+from sqlalchemy.schema import CreateIndex
+
 from pedsnetdcc.concept_name_transform import ConceptNameTransform
 
 
 def clean(s):
     """Strip leading & trailing space, remove newlines, compress space.
     Also expand '{NL}' to a literal newline.
+    :param str s:
+    :rtype: str
     """
     s = s.strip()
     s = re.sub(' +', ' ', s)
@@ -18,39 +23,47 @@ def clean(s):
 
 
 class ConceptNameTest(unittest.TestCase):
-    def test(self):
-        """
-        If you say so ...
-        """
-        metadata = MetaData()
 
-        col1 = Column('foo_concept_id', Integer)
-        col2 = Column('bar_concept_id', Integer)
+    def setUp(self):
+        self.metadata = MetaData()
 
-        user = Table('user', metadata,
-                     col1,
-                     col2)
+        foo_col = Column('foo_concept_id', Integer)
+        bar_col = Column('bar_concept_id', Integer)
+
+        self.table1 = Table('table1', self.metadata,
+                            foo_col,
+                            bar_col)
+
+        baz_col = Column('baz_concept_id', Integer)
+
+        self.table2 = Table('table2', self.metadata,
+                            baz_col)
 
         # Create and add the `concept` table to the sqlalchemy metadata
-        concept = Table('concept', metadata, Column('concept_id', Integer),
-                        Column('concept_name', String(512)))
+        self.concept = Table('concept', self.metadata,
+                             Column('concept_id', Integer),
+                             Column('concept_name', String(512)))
 
-        select_obj = select([user])
-        join_obj = user
+    def test_modify_select(self):
 
-        select_obj, join_obj = ConceptNameTransform.modify_select(metadata,
-                                                                  'user',
-                                                                  select_obj,
-                                                                  join_obj)
+        select_obj = select([self.table1])
+        join_obj = self.table1
+
+        select_obj, join_obj = ConceptNameTransform.modify_select(
+            self.metadata,
+            'user',
+            select_obj,
+            join_obj)
+
         select_obj = select_obj.select_from(join_obj)
 
         new_sql = str(select_obj.compile(dialect=postgresql.dialect()))
 
         expected = clean("""
-          SELECT "user".foo_concept_id, "user".bar_concept_id,
+          SELECT table1.foo_concept_id, table1.bar_concept_id,
           concept_1.concept_name AS foo_concept_name, concept_2.concept_name
           AS bar_concept_name
-          {NL}FROM "user"
+          {NL}FROM table1
           LEFT OUTER JOIN concept AS concept_1
               ON concept_1.concept_id = foo_concept_id
           LEFT OUTER JOIN concept AS concept_2
@@ -59,3 +72,21 @@ class ConceptNameTest(unittest.TestCase):
 
         self.maxDiff = None
         self.assertEqual(expected, new_sql)
+
+    def test_modify_metadata(self):
+
+        metadata = ConceptNameTransform.modify_metadata(self.metadata)
+
+        indexes = metadata.tables['table1'].indexes
+        self.assertEqual(len(indexes), 2, 'Indexes created')
+        num_expected_indexes = 0
+        for index in indexes:
+            index_sql = str(CreateIndex(index).compile(
+                dialect=postgresql.dialect()))
+            if index.name == 'tab_fcn_81387a6132eed7f69b5_ix':
+                self.assertEqual(index_sql, 'CREATE INDEX tab_fcn_81387a6132eed7f69b5_ix ON table1 (foo_concept_name)')
+                num_expected_indexes += 1
+            elif index.name == 'tab_bcn_7baa5e16ad1f8129b90_ix':
+                self.assertEqual(index_sql, 'CREATE INDEX tab_bcn_7baa5e16ad1f8129b90_ix ON table1 (bar_concept_name)')
+                num_expected_indexes += 1
+        self.assertEqual(num_expected_indexes, 2)
