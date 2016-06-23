95b292c8ddd5d4e44403ba7e17ff310187b55ab9
Kevin Murphy <murphyke@email.chop.edu>
2016-06-22 21:34:15 -0400

    Rename schema param of pre_transform to search_path

--- pedsnetdcc/abstract_transform.py
+++ pedsnetdcc/abstract_transform.py
@@ -28,14 +28,14 @@ class Transform(object):
     __metaclass__ = ABCMeta
 
     @classmethod
-    def pre_transform(cls, dburi, schema):
+    def pre_transform(cls, dburi, search_path):
         """Execute statements required before a transform.
 
         A Transform can override this method to execute prerequisite
         statements for a transform, e.g. creating database functions, etc.
 
-        :param str dburi:
-        :param str schema:
+        :param str dburi: URL to access database
+        :param str search_path: PostgreSQL schema search_path
         :return: None
         """
         pass
