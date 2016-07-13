f204ab40cc54ee696d3803c4d0321d73de56e840
Kevin Murphy <murphyke@email.chop.edu>
2016-07-13 15:21:22 -0400

    Address Aaron's initial review comments
    TODO: add statement execution.

--- pedsnetdcc/indexes.py
+++ pedsnetdcc/indexes.py
@@ -39,27 +39,6 @@ def _indexes_from_metadata(metadata, transforms, vocabulary=False):
     return indexes
 
 
-def _indexes_from_model_version(model_version, vocabulary=False):
-    """Return list of SQLAlchemy index objects for the transformed metadata.
-
-    Given the stock metadata, for each transform `T` we invoke:
-
-        new_metadata = T.modify_metadata(metadata)
-
-    and at the end, we extract the indexes.
-
-    :param model_version: pedsnet model version
-    :type: str
-    :param vocabulary: whether to return indexes for vocabulary tables or
-    non-vocabulary tables
-    :type: bool
-    :return: list of index objects
-    :rtype: list(sqlalchemy.Index)
-    """
-    return _indexes_from_metadata(stock_metadata(model_version), TRANSFORMS,
-                                  vocabulary=vocabulary)
-
-
 def indexes_sql(model_version, drop=False, vocabulary=False):
     """Create ADD or DROP INDEX statements for a transformed PEDSnet schema.
 
@@ -81,7 +60,8 @@ def indexes_sql(model_version, drop=False, vocabulary=False):
     :return: list of SQL ADD or DROP INDEX statements
     :type: list(str)
     """
-    indexes = _indexes_from_model_version(model_version, vocabulary=vocabulary)
+    indexes = _indexes_from_metadata(stock_metadata(model_version), TRANSFORMS,
+                                     vocabulary=vocabulary)
     if not drop:
         func = CreateIndex
     else:
