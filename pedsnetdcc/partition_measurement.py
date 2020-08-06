8f04794e7a9480aefe0f919e3b43e45dca99f40a
piglet25 <shague@stevens.edu>
2020-08-06 13:54:03 -0400

    added lab loinc columns

--- pedsnetdcc/partition_measurement.py
+++ pedsnetdcc/partition_measurement.py
@@ -168,30 +168,30 @@ ADD_INHERIT_SQL = 'ALTER TABLE {0}.measurement_{1} INHERIT {0}.measurement;'
 
 def _copy_to_measure_table(conn_str, table, concepts):
     copy_to_sql = """INSERT INTO measurement_{0}(
-        measurement_concept_id, measurement_date, measurement_datetime, measurement_id, 
-        measurement_order_date, measurement_order_datetime, measurement_result_date, 
-        measurement_result_datetime, measurement_source_concept_id, measurement_source_value, 
-        measurement_type_concept_id, operator_concept_id, person_id, priority_concept_id, 
-        priority_source_value, provider_id, range_high, range_high_operator_concept_id, 
-        range_high_source_value, range_low, range_low_operator_concept_id, range_low_source_value, 
-        specimen_source_value, unit_concept_id, unit_source_value, value_as_concept_id, 
-        value_as_number, value_source_value, visit_occurrence_id, measurement_age_in_months, 
-        measurement_result_age_in_months, measurement_concept_name, measurement_source_concept_name, 
-        measurement_type_concept_name, operator_concept_name, priority_concept_name, 
-        range_high_operator_concept_name, range_low_operator_concept_name, unit_concept_name, 
-        value_as_concept_name, site, site_id)
-        (select measurement_concept_id, measurement_date, measurement_datetime, measurement_id, 
-        measurement_order_date, measurement_order_datetime, measurement_result_date, 
-        measurement_result_datetime, measurement_source_concept_id, measurement_source_value, 
-        measurement_type_concept_id, operator_concept_id, person_id, priority_concept_id, 
-        priority_source_value, provider_id, range_high, range_high_operator_concept_id, 
-        range_high_source_value, range_low, range_low_operator_concept_id, range_low_source_value, 
-        specimen_source_value, unit_concept_id, unit_source_value, value_as_concept_id, 
-        value_as_number, value_source_value, visit_occurrence_id, measurement_age_in_months, 
-        measurement_result_age_in_months, measurement_concept_name, measurement_source_concept_name, 
-        measurement_type_concept_name, operator_concept_name, priority_concept_name, 
-        range_high_operator_concept_name, range_low_operator_concept_name, unit_concept_name, 
-        value_as_concept_name, site, site_id
+        measurement_concept_id, measurement_date, measurement_datetime, measurement_order_date, 
+        measurement_order_datetime, measurement_result_date, measurement_result_datetime, 
+        measurement_source_concept_id, measurement_source_value, measurement_type_concept_id, 
+        operator_concept_id, priority_concept_id, priority_source_value, range_high, 
+        range_high_operator_concept_id, range_high_source_value, range_low, range_low_operator_concept_id,
+        range_low_source_value, specimen_concept_id, specimen_source_value, unit_concept_id, 
+        unit_source_value, value_as_concept_id, value_as_number, value_source_value, 
+        measurement_age_in_months, measurement_result_age_in_months, measurement_concept_name, 
+        measurement_source_concept_name, measurement_type_concept_name, operator_concept_name, 
+        priority_concept_name, range_high_operator_concept_name, range_low_operator_concept_name, 
+        specimen_concept_name, unit_concept_name, value_as_concept_name, site, measurement_id,
+        site_id, provider_id, person_id, visit_occurrence_id)
+        (select measurement_concept_id, measurement_date, measurement_datetime, measurement_order_date, 
+        measurement_order_datetime, measurement_result_date, measurement_result_datetime, 
+        measurement_source_concept_id, measurement_source_value, measurement_type_concept_id, 
+        operator_concept_id, priority_concept_id, priority_source_value, range_high, 
+        range_high_operator_concept_id, range_high_source_value, range_low, range_low_operator_concept_id,
+        range_low_source_value, specimen_concept_id, specimen_source_value, unit_concept_id, 
+        unit_source_value, value_as_concept_id, value_as_number, value_source_value, 
+        measurement_age_in_months, measurement_result_age_in_months, measurement_concept_name, 
+        measurement_source_concept_name, measurement_type_concept_name, operator_concept_name, 
+        priority_concept_name, range_high_operator_concept_name, range_low_operator_concept_name, 
+        specimen_concept_name, unit_concept_name, value_as_concept_name, site, measurement_id,
+        site_id, provider_id, person_id, visit_occurrence_id)
         FROM measurement_anthro WHERE measurement_concept_id IN ({1})) ON CONFLICT DO NOTHING"""
 
     copy_to_msg = "copying {0} to measurement_{0}"
