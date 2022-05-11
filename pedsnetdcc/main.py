3f0542241da14286ae806c35b62e45ba14573e87
Hague <slhague@verizon.net>
2022-05-11 18:18:03 -0400

    testing output sql fot transform

--- pedsnetdcc/main.py
+++ pedsnetdcc/main.py
@@ -2690,9 +2690,13 @@ def run_recover_cohort(pwprompt, searchpath, site, model_version, dburi):
 @click.option('--searchpath', '-s', required=True)
 @click.option('--site', required=True,
               help='PEDSnet site name to add to tables.')
+@click.option('--name', required=False, default='dcc',
+              help='name of the id (ex: onco')
+@click.option('--stringid', is_flag=True, default=False,
+              help='Use string type for ids.')
 @click.option('--model-version', '-v', required=True,
               help='PEDSnet model version (e.g. 2.3.0).')
-def generate_transform_statements(dburi, pwprompt, searchpath, model_version, site):
+def generate_transform_statements(dburi, pwprompt, searchpath, model_version, name, stringid, site):
     from pedsnetdcc.transform_runner import _transform_select_sql
     from pedsnetdcc.schema import create_schema, primary_schema
     if pwprompt:
@@ -2700,9 +2704,13 @@ def generate_transform_statements(dburi, pwprompt, searchpath, model_version, si
 
     # TODO: do we need to validate the primary schema at all?
     schema = primary_schema(searchpath)
+    if stringid:
+        idtype = 'String'
+    else:
+        idtype = 'BigInteger'
 
     tmp_schema = schema + '_' + 'transformed'
-    for sql, msg in _transform_select_sql(model_version, site, tmp_schema):
+    for sql, msg in _transform_select_sql(model_version, site, tmp_schema, name, idtype):
         print("msg: " + sql)
 
 
